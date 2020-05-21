b338670cf7b26bfbcc0fcaad49ab11b6ebf744d2
eager-class-5814 <candacehchoe@gmail.com>
2020-05-21 03:21:47 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,7 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    puts "Happy birthday #{kids_name}! 
+    puts "Happy birthday #{kids_name}! \n
     You are now #{age} years old!"
   end 
 end
